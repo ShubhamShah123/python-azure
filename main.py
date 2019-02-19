b2020f39a4a726f6124374094427d6269f58ae1f
Skynox <skynoxcrypt@gmail.com>
2019-02-19 04:06:37 -0600

    templates folder

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-# -*- coding: utf-8 -*-
-# Librarys
-from flask import Flask, render_template, jsonify
-import os
-# Variables
+from flask import Flask
 app = Flask(__name__)
 
-# Settings
-
-
-# Views
-@app.route('/', methods=['GET'])
-def index():
-	return render_template('index.html')
-
-
-# Run
-if __name__ == '__main__':
-	port = int(os.getenv('PORT', 8000))
-	app.run(host='0.0.0.0', port=port, debug=True)
-	# app.run(host='10.219.138.53', port=port, debug=True)
+@app.route("/")
+def hello():
+    return "Hello World!"
