be79e7b9a06c5131d76d80e8c4976d2b90409c64
Skynox <skynoxcrypt@gmail.com>
2019-02-19 04:08:29 -0600

    change

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+# Librarys
+from flask import Flask, render_template, jsonify
+import os
+# Variables
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Settings
+
+
+# Views
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+
+# Run
+if __name__ == '__main__':
+	port = int(os.getenv('PORT', 8000))
+	app.run(host='0.0.0.0', port=port, debug=True)
+	# app.run(host='10.219.138.53', port=port, debug=True)
