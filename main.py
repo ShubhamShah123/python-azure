7f926eda9fe1e9d18d32899053eae99550f95f9b
Skynox <skynoxcrypt@gmail.com>
2019-02-20 16:19:59 -0600

    Port added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,5 @@ def hello_world():
   return 'Hey its Python Flask application!'
 
 if __name__ == '__main__':
-  app.run()
+  port = int(os.getenv('PORT', 8000))
+  app.run(host='0.0.0.0', port=port, debug=True)
