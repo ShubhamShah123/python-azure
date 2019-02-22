cd80eb279cb5e9782c8fa7498e67a28d72948dd2
Skynox <skynoxcrypt@gmail.com>
2019-02-22 14:26:12 -0600

    Added index.html

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  print "Hello World"
-  return 'Hey its Python Flask application!'
+  return render_template('index.html', name = "Shah Shubham", net_id = "sbs8554")
 
 if __name__ == '__main__':
   port = int(os.getenv('PORT', 8000))
